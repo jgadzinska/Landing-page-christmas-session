b9b8176aa2eddf7c8156fd5822ab51326a54ded4
jgadzinska <joanna.gadzinska88@wp.pl>
2023-12-16 12:33:56 +0100

    New app.js

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ document.querySelector("#menu").onclick = () => {
 document.querySelector("#close").onclick = () => {
     navbar.classList.remove("active");
 };
+
+
+for (let i=0; i < 100; i++) {
+    let snow = document.createElement('div');
+    
+    let po = Math.floor(Math.random()*100);
+    let d = Math.floor(Math.random()*10000);
+
+    let snowBlur = Math.floor(Math.random()*13);
+
+    snow.style.left = po + '%';
+    snow.style.animationDuration = d + 2000 + 'ms';
+    snow.style.filter = 'blur(${snowBlur+2}px)';
+
+    snow.id = 'snow';
+
+    document.body.appendChild(snow); 
+};
